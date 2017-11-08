b4bbfb8d17f48d6ad156acf78270122de622ebeb
guilofrano <gcorneto@hotmail.com>
2017-11-08 01:28:40 -0200

    Initial

---
+++ AsyncHost/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace AsyncHost
+{
+    public class Class1
+    {
+    }
+}
---
+++ AsyncTcpConnection/AsyncHost.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace AsyncTcpConnection
+{
+    public class AsyncHost
+    {
+    }
+}
